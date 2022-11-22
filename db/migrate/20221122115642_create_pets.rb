81e14719c5d177d2f53507d3e1a938a1abb73c22
Saida Murtazali <saida.murtazaliyeva@yahoo.com>
2022-11-22 11:57:55 +0000

    created model pets

---
+++ db/migrate/20221122115642_create_pets.rb
@@ -0,0 +1,9 @@
+class CreatePets < ActiveRecord::Migration[7.0]
+  def change
+    create_table :pets do |t|
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
