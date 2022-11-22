8e4606eec73ac9a8cc0e54aaf70e4f432a584a86
Saida Murtazali <saida.murtazaliyeva@yahoo.com>
2022-11-22 12:03:00 +0000

    destroyed model pet

--- db/migrate/20221122115642_create_pets.rb
+++
@@ -1,9 +0,0 @@
-class CreatePets < ActiveRecord::Migration[7.0]
-  def change
-    create_table :pets do |t|
-      t.references :user, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
