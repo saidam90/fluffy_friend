cbf0aadf76e11c77eefc5eb57c539a86a88035bb
Saida Murtazali <saida.murtazaliyeva@yahoo.com>
2022-11-22 14:14:20 +0000

    tested a User and Pet models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :pets
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\A.*@.*\..*\z/ }
+  # validates :owner, inclusion: { in: [true, false] }
 end
--- db/migrate/20221122120549_create_pets.rb
+++ db/migrate/20221122120549_create_pets.rb
@@ -1,6 +1,12 @@
 class CreatePets < ActiveRecord::Migration[7.0]
   def change
     create_table :pets do |t|
+      t.string :category
+      t.string :breed
+      t.string :name
+      t.integer :age
+      t.string :location
+      t.string :photo
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
