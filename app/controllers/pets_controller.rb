a149aeb8cefe448978d6b0f8bba356214e2db0c0
Saida Murtazali <saida.murtazaliyeva@yahoo.com>
2022-11-25 12:32:29 +0000

    fixed css and added the map

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,19 +1,12 @@
 class PetsController < ApplicationController
   def index
     @pets = Pet.all
-  end
-
-  def create
-    @pet = Pet.new(pet_params)
-    if @pet.save
-      redirect_to pets_path
-    else
-      render :new, status: :unprocessable_entity
-    end
+    @reviews = Review.all
   end
 
   def show
     @pet = Pet.find(params[:id])
+    @reviews = Review.where(pet_id: @pet.reviews)
     @marker = [{
       lat: @pet.latitude,
       lng: @pet.longitude,
@@ -21,9 +14,22 @@ class PetsController < ApplicationController
     }]
   end
 
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      redirect_to pets_path, notice: " #{@pet.name} added successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def list_params
-    params.require(:pet).permit(:category, :breed, :name, :location)
+    params.require(:pet).permit(:category, :breed, :name, :age, :location, photos: [])
   end
 end
