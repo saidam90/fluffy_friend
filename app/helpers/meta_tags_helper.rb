66910942057c536bfc12e6e8d3ecca0f0c3c0faa
Saida Murtazali <saida.murtazaliyeva@yahoo.com>
2023-01-04 18:38:01 +0000

    added social meta tags

---
+++ app/helpers/meta_tags_helper.rb
@@ -0,0 +1,9 @@
+module MetaTagsHelper
+  def meta_title
+    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["meta_title"]
+  end
+
+  def meta_description
+    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["meta_description"]
+  end
+end
