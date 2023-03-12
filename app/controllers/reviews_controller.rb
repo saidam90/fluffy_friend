8d0384863f7557e2ca8ff8e9554feeb673832f96
Saida Murtazali <saida.murtazaliyeva@yahoo.com>
2023-03-12 18:42:11 +0400

    fixed rating issue

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,20 @@
 class ReviewsController < ApplicationController
 before_action :set_pet, only: %i[new create]
 
+  def new
+    @review = Review.new
+  end
+
   def create
     @review = Review.new(review_params)
     @review.pet = @pet
-    @review.save
-    redirect_to pet_path(@pet)
-  end
 
-  def new
-    @review = Review.new
+    if @review.rating < 1 || @review.rating > 5
+      "Should be between 1 to 5"
+    else
+      @review.save
+    end
+    redirect_to pet_path(@pet)
   end
 
   def index
@@ -17,17 +22,17 @@ before_action :set_pet, only: %i[new create]
     @reviews = Review.where(pet_id: @pet.reviews)
   end
 
-  private
+  def show
+    @review = Review.find
+  end
 
   def set_pet
     @pet = Pet.find(params[:pet_id])
   end
 
+  private
+
   def review_params
     params.require(:review).permit(:rating, :comment)
-  end
-
-  def show
-    @review = Review.find
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :pet
 
-  validates :rating, presence: true
-  validates :comment, presence: true
+  validates :rating, :inclusion => 1..5
+  validates :comment, :presence => true
 end
